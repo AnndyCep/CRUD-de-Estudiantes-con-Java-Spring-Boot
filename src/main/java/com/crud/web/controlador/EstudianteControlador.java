815727b3999f46b7ff3375c86465ae7f883b4106
AnndyCep <hannsonandrey@hotmail.com>
2024-02-09 12:52:47 -0500

    Subiendo formulario actualizar, este ya lista el estudiante, y lo actualiza en la lista

--- src/main/java/com/crud/web/controlador/EstudianteControlador.java
+++ src/main/java/com/crud/web/controlador/EstudianteControlador.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,14 +20,14 @@ public class EstudianteControlador {
     
     @Autowired
     private EstudianteServicio servicio;
-    
+    //Listar los estudiantes
     @GetMapping
     public String listarEstudiantes(Model model){
         List<Estudiante> listaEstudiante = servicio.listarTodos();
         model.addAttribute("listaEstudiantes", listaEstudiante);
         return "estudiantes";  //retorna la el template.
     }
-    
+    //Mostrar el formulario  para crear estudiante
     @GetMapping("/nueva")
     public String nuevoEstudiante(Model model){
         //Creamos un objet vacion
@@ -36,10 +37,24 @@ public class EstudianteControlador {
         
         return "formulario";
     }
-    
+    //Metodo para guardar el estudiante en la bbdd
     @PostMapping("/nueva")
     public String guardarEstudiante(@ModelAttribute Estudiante estudiante){
         servicio.crearEstudiante(estudiante);
+        return "redirect:/";
+    }
+    //ostar formulario para actualizar datos de usuario
+    @GetMapping("/editar/{id}")
+    public String mostrarEditarEstudiante(@PathVariable int id, Model model){
+        Estudiante estudiante = servicio.encontrarEstudinte(id);
+        model.addAttribute("estudiante", estudiante);
+        model.addAttribute("accion", "/editar/" + id);
+        return "actualizar_estudiante";
+    }
+    //Actulizar los datos que se envias desde el formulario
+    @PostMapping("/editar/{id}")
+    public String actualizarEstudiante(@PathVariable int id, @ModelAttribute Estudiante estudiante){
+        servicio.actualizarEstudiante(id, estudiante);
         return "redirect:/";
     }
 }
