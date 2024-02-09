3ec0724287f751c77cb7161370909acb0868350f
AnndyCep <hannsonandrey@hotmail.com>
2024-02-09 11:33:26 -0500

    Crear usuario como formulario, se lista estudiantes

--- src/main/java/com/crud/web/controlador/EstudianteControlador.java
+++ src/main/java/com/crud/web/controlador/EstudianteControlador.java
@@ -3,10 +3,13 @@ package com.crud.web.controlador;
 
 import com.crud.web.entities.Estudiante;
 import com.crud.web.servicio.EstudianteServicio;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,18 +22,24 @@ public class EstudianteControlador {
     
     @GetMapping
     public String listarEstudiantes(Model model){
-        
-        model.addAttribute("estudiante",servicio.listarTodos());
+        List<Estudiante> listaEstudiante = servicio.listarTodos();
+        model.addAttribute("listaEstudiantes", listaEstudiante);
         return "estudiantes";  //retorna la el template.
     }
     
-    @GetMapping("/nuevo")
+    @GetMapping("/nueva")
     public String nuevoEstudiante(Model model){
         //Creamos un objet vacion
         Estudiante estudiante = new Estudiante();
+        model.addAttribute("estudiante", estudiante);
+        model.addAttribute("accion","/nueva");
         
-        
-        return "";
+        return "formulario";
     }
     
+    @PostMapping("/nueva")
+    public String guardarEstudiante(@ModelAttribute Estudiante estudiante){
+        servicio.crearEstudiante(estudiante);
+        return "redirect:/";
+    }
 }
